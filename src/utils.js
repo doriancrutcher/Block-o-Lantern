795c2d90e19c6f3c0daaf89602de54df7026a092
Dorian Crutcher <doriancrutcher@Dorians-MacBook-Pro.local>
2020-10-28 14:26:14 -0700

    changed config to testnet

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import { connect, Contract, keyStores, WalletConnection, Account } from 'near-api-js'
 import getConfig from './config'
 
-const nearConfig = getConfig(process.env.NODE_ENV || 'mainnet')
+const nearConfig = getConfig(process.env.NODE_ENV || 'testnet')
 
 // Initialize contract & set global variables
 export async function initContract() {
