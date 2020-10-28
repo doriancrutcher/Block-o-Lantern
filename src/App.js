310012f9a47b22fc574728c178d0772ecbffec70
Dorian Crutcher <doriancrutcher@Dorians-MacBook-Pro.local>
2020-10-28 15:27:47 -0700

    fixed logo link

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
   return (
    <React.Fragment>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand className="ScaryStyleFont" href="/">Block '0' Lantern </Navbar.Brand>
+        <Navbar.Brand className="ScaryStyleFont" ><Link to='/'>Block '0' Lantern </Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
